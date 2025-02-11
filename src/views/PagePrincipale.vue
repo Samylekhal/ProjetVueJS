ae7331b287bc1efcb1e743ad8258adb43457f8a1
Samylekhal <samylekhal145@gmail.com>
2025-02-11 09:17:50 +0100

    préparation des pages

---
+++ src/views/PagePrincipale.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import DemPlateau from '@/components/DemPlateau.vue'
+const difficult = ['Facile', 'Moyen', 'Difficile']
+
+const bouton = ref(false)
+
+const jouer = () => {
+  bouton.value = !bouton.value
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Démineur</h1>
+      <p>Un projet de Samy Lekhal</p>
+      <br />
+      <nav>
+        <ul>
+          <li>
+            <button @click="jouer">Jouer</button>
+          </li>
+        </ul>
+      </nav>
+
+      <div v-if="bouton">
+        <h4>Difficulté</h4>
+        <ul>
+          <li v-for="n in difficult" :key="n">
+            <button>{{ n }}</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
